2d8f17d875d4319bafbc2491fdb37a5c4ddcd905
Phung Duong <daibbcvl@yahoo.com>
2019-05-28 15:51:46 +0700

    Seperate admin and front end css

--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,46 @@ Encore
     .cleanupOutputBeforeBuild()
     .autoProvidejQuery()
     .enableVersioning()
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', './assets/admin/js/app.js')
     .enableSingleRuntimeChunk()
     .addRule({
-        test: require.resolve('./assets/js/vendors.bundle.js'),
+        test: require.resolve('./assets/admin/js/vendors.bundle.js'),
         use: ['script-loader']
     })
     .addRule({
-        test: require.resolve('./assets/js/scripts.bundle.js'),
+        test: require.resolve('./assets/admin/js/scripts.bundle.js'),
         use: ['script-loader']
     })
 ;
 
-module.exports = Encore.getWebpackConfig();
+// build the admin configuration
+const admin = Encore.getWebpackConfig();
+
+// Set a unique name for the config (needed later!)
+admin.name = 'admin';
+
+// reset Encore to build the second config
+Encore.reset();
+
+/**
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('mobile', './assets/js/mobile.js')
+    .addStyleEntry('mobile', './assets/css/mobile.less')
+    .enableLessLoader()
+    .enableSourceMaps(!Encore.isProduction())
+;
+
+// build the second configuration
+const secondConfig = Encore.getWebpackConfig();
+
+// Set a unique name for the config (needed later!)
+secondConfig.name = 'secondConfig';
+
+ **/
+
+// export the final configuration as an array of multiple configurations
+module.exports = [admin];
+
+// Config for front end will implement later
