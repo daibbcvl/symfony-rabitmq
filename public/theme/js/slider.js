328b32beac86bf5af12003e7f5e9894ed5e602df
Phung Duong <phung.duong@nfq.asia>
2019-09-30 22:09:17 +0700

    Tour trong nuoc + blog details

--- public/theme/js/slider.js
+++ public/theme/js/slider.js
@@ -1,11 +1,22 @@
-$(document).ready(function() {
- $(".regular").slick({
+$(document).ready(function () {
+    $(".regular").slick({
         dots: false,
         infinite: true,
         slidesToShow: 5,
         slidesToScroll: 3,
         nextArrow: '<button class="product-widget-arrows product-widget-arrow-right ng-star-inserted" style="top: 50%;"><i class="material-icons">chevron_right</i></button>',
         prevArrow: '<button class="product-widget-arrows product-widget-arrow-left ng-star-inserted" style="top: 50%;"><i class="material-icons">chevron_left</i></button>'
-      });
+    });
+
+    $(".slide-three").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        nextArrow: '<button class="product-widget-arrows product-widget-arrow-right ng-star-inserted" style="top: 50%;"><i class="material-icons">chevron_right</i></button>',
+        prevArrow: '<button class="product-widget-arrows product-widget-arrow-left ng-star-inserted" style="top: 50%;"><i class="material-icons">chevron_left</i></button>'
+    });
+
+
 });
 
