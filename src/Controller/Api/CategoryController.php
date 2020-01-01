df618e99876c131042dd851ce92a661f388b26ee
Phung Duong <phung.duong@nfq.asia>
2020-01-01 20:12:24 +0700

    update abroad category

--- src/Controller/Api/CategoryController.php
+++ src/Controller/Api/CategoryController.php
@@ -54,7 +54,6 @@ class CategoryController extends AbstractController
         $domesticCategoryResult->addItem($middle);
         $domesticCategoryResult->addItem($south);
 
-
         $response = $this->json($this->toJsonSerializable($domesticCategoryResult));
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
@@ -63,7 +62,63 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * @Route("/api/categories/{categorySlug}", name="api_category_abroad")
+     * @Route("/api/categories/abroad", name="api_category_abroad")
+     *
+     * @param PostRepository     $postRepository
+     *
+     * @param CategoryRepository $categoryRepository
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function abroad(PostRepository $postRepository, CategoryRepository $categoryRepository)
+    {
+        $domesticCategory = $categoryRepository->findOneByCategorySlug('di-nuoc-ngoai');
+        $northCategory = $categoryRepository->findOneByCategorySlug('chau-a');
+        $middleCategory = $categoryRepository->findOneByCategorySlug('chau-au');
+        $southCategory = $categoryRepository->findOneByCategorySlug('chau-my');
+        $africaCategory = $categoryRepository->findOneByCategorySlug('chau-phi');
+        $australiaCategory = $categoryRepository->findOneByCategorySlug('chau-uc');
+
+        $domesticCategoryResult = new DomesticCategory($domesticCategory->getName(), $domesticCategory->getCategorySlug());
+        $north = new CategoryItem($northCategory->getName());
+        $north->setSlug($northCategory->getCategorySlug());
+        $north->setPosts($postRepository->getPostByCategory($northCategory, 3));
+
+        $middle = new CategoryItem($middleCategory->getName());
+        $middle->setPosts($postRepository->getPostByCategory($middleCategory, 3));
+        $middle->setSlug($middleCategory->getCategorySlug());
+
+        $south = new CategoryItem($southCategory->getName());
+        $south->setPosts($postRepository->getPostByCategory($southCategory, 3));
+        $south->setSlug($southCategory->getCategorySlug());
+
+        $south = new CategoryItem($southCategory->getName());
+        $south->setPosts($postRepository->getPostByCategory($southCategory, 3));
+        $south->setSlug($southCategory->getCategorySlug());
+
+        $africa = new CategoryItem($africaCategory->getName());
+        $africa->setPosts($postRepository->getPostByCategory($africaCategory, 3));
+        $africa->setSlug($africaCategory->getCategorySlug());
+
+        $australia = new CategoryItem($australiaCategory->getName());
+        $australia->setPosts($postRepository->getPostByCategory($australiaCategory, 3));
+        $australia->setSlug($australiaCategory->getCategorySlug());
+
+        $domesticCategoryResult->addItem($north);
+        $domesticCategoryResult->addItem($middle);
+        $domesticCategoryResult->addItem($south);
+        $domesticCategoryResult->addItem($africa);
+        $domesticCategoryResult->addItem($australia);
+
+        $response = $this->json($this->toJsonSerializable($domesticCategoryResult));
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        return $response;
+    }
+
+
+    /**
+     * @Route("/api/categories/{categorySlug}", name="api_category")
      *
      * @param Category       $category
      * @param PostRepository $postRepository
@@ -71,7 +126,7 @@ class CategoryController extends AbstractController
      * @param Request        $request
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function abroad(Category $category, PostRepository $postRepository, Request $request)
+    public function category(Category $category, PostRepository $postRepository, Request $request)
     {
         $result = $this->getArea($category->getCategorySlug(), $request, $postRepository);
         $response = $this->json($this->toJsonSerializable($result));
